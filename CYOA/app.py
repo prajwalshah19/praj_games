aa2bbdfd930a6c83c9d257a71deeb465f52e8b40
prajwalshah19 <70662078+prajwalshah19@users.noreply.github.com>
2022-04-14 16:41:48 -0500

    4/14

---
+++ CYOA/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    title = "The Beginning..."
+
+    text = """One day on the way home from school, you decide to take a shortcut through the forest. 
+	Walking through the trees and vines, it starts to get dark. Eventually stumble onto a sign.
+	It reads DO NOT ENTER: DANGER AHEAD."""
+
+    return render_template("template.html" , title = title, text = text)
+
+
