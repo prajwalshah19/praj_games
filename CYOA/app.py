76fabe952c4482832a41abf2ed92eed68c868772
prajwalshah19 <70662078+prajwalshah19@users.noreply.github.com>
2022-04-14 17:43:59 -0500

    4/14

--- CYOA/app.py
+++ CYOA/app.py
@@ -8,9 +8,9 @@ def home():
     title = "The Beginning..."
 
     text = """One day on the way home from school, you decide to take a shortcut through the forest. 
-	Walking through the trees and vines, it starts to get dark. Eventually stumble onto a sign.
+	Walking through the trees and vines, it starts to get dark. Eventually you stumble upon a sign.
 	It reads DO NOT ENTER: DANGER AHEAD."""
 
-    return render_template("template.html" , title = title, text = text)
+    return render_template('template.html' , title = title, text = text)
 
 
