e3022ad09a7e29579e3e1ffb03d3447f46c27f0a
prajwalshah19 <70662078+prajwalshah19@users.noreply.github.com>
2022-05-27 15:18:42 -0500

    5/27

--- CYOA/app.py
+++ CYOA/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +12,18 @@ def home():
 	Walking through the trees and vines, it starts to get dark. Eventually you stumble upon a sign.
 	It reads DO NOT ENTER: DANGER AHEAD."""
 
-    return render_template('template.html' , title = title, text = text)
+    img = "/static/page1.jpg"
 
+    choices = [("Enter Forest", "Ignore the sign, go into the forest"), ("RIP", "Turn around")]
 
+    return render_template('template.html' , title = title, text = text, img = img, choices = choices)
+
+@app.route("/Enter Forest")
+def page3():
+
+    return render_template('template.html')
+
+@app.route("/RIP")
+def page2():
+
+    return render_template('template.html')
